c3af186915d920851ec2627fb0c70ed8ea4fa96f
Ganzp <ganzp@naver.com>
2020-02-22 15:29:30 +0900

    first commit

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from "typeorm"
 
 const connectionOptions: ConnectionOptions = {
 	type: "postgres",
-	database: "reco",
+	database: process.env.DB_NAME || "reco",
 	synchronize: true,
 	logging: true,
 	entities: ["entities/*.*"],
